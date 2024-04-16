91349f4744bc02bb60ee10e504f548a0493c0e30
Konrad <vobis08gw@gmail.com>
2024-04-16 23:51:41 +0200

    Update page.tsx

--- guildhub/app/faq/page.tsx
+++ guildhub/app/faq/page.tsx
@@ -5,48 +5,20 @@ const faq = () => {
     <div>
       <p>faq.tsx</p>
       <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
+        question="What is GuildHub?"
+        respond="GuildHub is Albion Online's guild manager. A set of tools designed to support the management of a guild, its members, officers, blacklist, treasury, and a mass of other things."
       />
       <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
+        question="Can I use GuildHub for another game?"
+        respond="All of the tools available in the app are designed specifically for Albion Online, but if you find them useful, feel free to use them!"
       />
       <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
+        question="Is there support planned for other games?"
+        respond="At this point, support for other games is in distant plans, but it is being considered. Priority is given to the development of Albion applications, with World of Warcraft and ESO as potential future additions."
       />
       <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
-      />
-      <Question
-        question="Question"
-        respond="respondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrespondrendrespondrespondrespondrespondrespondrespondrespondrespondrespond end"
+        question="Is the platform free of charge?"
+        respond="At this moment, yes, the platform is free of charge. However, there are plans to move to a subscription model with a free plan in the future."
       />
     </div>
   );
