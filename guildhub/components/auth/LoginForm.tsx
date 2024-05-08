4ab99ab6c81180aed37548c5344dc342d61b5514
Konrad <vobis08gw@gmail.com>
2024-05-08 20:59:01 +0200

    Update LoginForm.tsx

--- guildhub/components/auth/LoginForm.tsx
+++ guildhub/components/auth/LoginForm.tsx
@@ -13,7 +13,16 @@ const LoginForm: React.FC = () => {
         <p className="txt-desc">Tell us more about you</p>
       </div>
       <div className="wrapper mid">
-        <form action="" className="login-form"></form>
+        <form action="" className="login-form">
+          <label htmlFor="tag" className="login-form-label">
+            Acess tag
+          </label>
+          <input name="tag" type="text"></input>
+          <label htmlFor="token" className="login-form-label">
+            Acess token
+          </label>
+          <input name="token" type="text"></input>
+        </form>
       </div>
       <div className="wrapper bot">
         <a className="wrapper-bot-link" href="/faq">
