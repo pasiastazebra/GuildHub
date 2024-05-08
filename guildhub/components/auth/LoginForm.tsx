e11d3626bf4e6c7fc572c3fc286c0dd2ed827fd1
Konrad <vobis08gw@gmail.com>
2024-05-08 21:11:15 +0200

    uperwrapper

--- guildhub/components/auth/LoginForm.tsx
+++ guildhub/components/auth/LoginForm.tsx
@@ -8,21 +8,27 @@ import "@/styles/loginForm.scss";
 const LoginForm: React.FC = () => {
   return (
     <div className="app">
-      <div className="wrapper top">
-        <h1 className="txt-title">Welcome back</h1>
-        <p className="txt-desc">Tell us more about you</p>
-      </div>
-      <div className="wrapper mid">
-        <form action="" className="login-form">
-          <label htmlFor="tag" className="login-form-label">
-            Acess tag
-          </label>
-          <input name="tag" type="text"></input>
-          <label htmlFor="token" className="login-form-label">
-            Acess token
-          </label>
-          <input name="token" type="text"></input>
-        </form>
+      <div className="uperwrapper">
+        <div className="wrapper top">
+          <h1 className="txt-title">Welcome back</h1>
+          <p className="txt-desc">Tell us more about you</p>
+        </div>
+        <div className="wrapper mid">
+          <form action="" className="login-form">
+            <label htmlFor="tag" className="login-form-label">
+              Acess tag
+            </label>
+            <input name="tag" type="text" className="login-form-input"></input>
+            <label htmlFor="token" className="login-form-label">
+              Acess token
+            </label>
+            <input
+              name="token"
+              type="text"
+              className="login-form-input"
+            ></input>
+          </form>
+        </div>
       </div>
       <div className="wrapper bot">
         <a className="wrapper-bot-link" href="/faq">
